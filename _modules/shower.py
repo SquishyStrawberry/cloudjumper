f7d5908fa9f6b8eee4821b3ac7a76cf64723b010
SquishyStrawberry <therealrebuilder@gmail.com>
2015-08-08 21:14:45 +0200

    Re-Added modules

---
+++ _modules/shower.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+class Shower(object):
+    
+    def __init__(self, bot, config):
+        self.bot = bot
+        self.bot.subscribe(publisher=self.bot.MESSAGE,
+                           handler=self.print,
+                           command="modules")
+
+    def print(self, sender, args):
+        msg = self.bot.get_message("list_modules")
+        msg = msg.format(modules=", ".join(i.__name__ for i in self.bot.modules))
+        self.bot.send_action(msg)
--- modules.py
+++ modules.py
@@ -4,10 +4,12 @@ from _modules.greeter_attacker import Attacker
 from _modules.stomach          import Stomach
 from _modules.calc             import Calculator
 from _modules.terminate        import Terminator
+from _modules.shower           import Shower
 
 modules = (
     Terminator,
     Greeter,
+    Shower,
     Attacker,
     Stomach,
     Calculator,
