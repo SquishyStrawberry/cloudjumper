5514c5aa49778cac27ba275ecaa895af1efdd42b
SquishyStrawberry <therealrebuilder@gmail.com>
2015-08-08 20:20:07 +0200

    Added Stomach

---
+++ _modules/stomach.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import collections
+
+
+class Stomach(object):
+    name = "stomach"
+
+    def __init__(self, bot, config):
+        self.bot     = bot
+        self.stomach = collections.OrderedDict()
+        self.bot.subscribe(publisher=self.bot.MESSAGE,
+                           handler=self.show_stomach,
+                           command="stomach")
+        self.bot.subscribe(publisher=self.bot.MESSAGE,
+                           handler=self.vomit,
+                           command="vomit")
+        self.bot.subscribe(publisher=self.bot.MESSAGE,
+                           handler=self.eat,
+                           command="eat")
+        self.bot.subscribe(publisher=self.bot.MESSAGE,
+                           handler=self.spit,
+                           command="spit")
+
+    def eat(self, sender, command, args):
+        victim = " " .join(args)
+        if victim.lower() not in self.stomach:
+            msg = self.bot.get_message("eat")
+        else:
+            msg = self.bot.get_message("eat_superfluous")   
+        self.bot.send_action(msg.format(victim=victim))
+        self.stomach[victim.lower()] = victim
+
+    def spit(self, sender, command, args):
+        victim = " " .join(args)
+        if victim.lower() in self.stomach:
+            msg = self.bot.get_message("spit")
+            del self.stomach[victim.lower()]
+        else:
+            msg = self.bot.get_message("spit_superfluous")
+
+        self.bot.send_action(msg.format(victim=victim))
+
+    def vomit(self, sender, command, args):
+        if self.stomach:
+            self.stomach = collections.OrderedDict()
+            msg = self.bot.get_message("vomit")
+        else:
+            msg = self.bot.get_message("vomit_superfluous")
+        self.bot.send_action(msg)
+
+    def show_stomach(self, sender, command, args):
+        stomach = list(self.stomach.values())
+        msg     = self.bot.get_message("stomach")
+        if len(stomach) < 1:
+            msg = self.bot.get_message("stomach_empty")
+            victims = ""
+        elif len(stomach) == 1:
+            victims = "just {}".format(stomach[0])
+        else:
+            victims = ", ".join(stomach[:-1])    
+            victims += ", and {}".format(stomach[-1])
+        self.bot.send_action(msg.format(victims=victims))
+        
+
--- modules.py
+++ modules.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from _modules.greeter_attacker import Greeter, Attacker
+from _modules.stomach import Stomach
 
-modules = (Greeter, Attacker)
 
+modules = (Greeter, Attacker, Stomach)
